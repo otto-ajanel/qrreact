18ae5a80dfdc349a8d6a2cf7a24ac60f738924d1
Otto Ajanel <ottonielajanel@gmail.com>
2023-10-05 05:17:09 +0000

    firts comias

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <img src="src/assets/images/image-qr-code.png" alt="QR" />
+        <h2>Improve youo front-end skills by building projets</h2>
+        <p>Scan the QR code to visit Frontend
+        <br />
+        Mentor and take your coding skills to the next level.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
